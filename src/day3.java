f9a2ae9a323041b08d3c0a0fe85291d2a69be101
Aishwariya <aishwariyak25csc@vetias.ac.in>
2026-06-08 14:23:01 +0530

    day3

--- src/day3.java
+++ src/day3.java
@@ -1,4 +1,4 @@
-void main(){
+/*void main(){
     int n=5;
     for(int row=1;row<=n;row++){
         for(int column=1;column<=row;column++){
@@ -7,4 +7,20 @@ void main(){
         }
         System.out.println();
     }
-}
+} */
+void main(){
+    int []marks={87,32,44,89,32};
+    System.out.println("pass mark");
+    for (int mark:marks){
+        if (mark >= 40) {
+            System.out.println(mark +"");
+        }
+        }
+    System.out.println("fail marks");
+    for(int mark:marks){
+        if(mark<40){
+            System.out.println(mark+"");
+        }
+    }
+    }
+
