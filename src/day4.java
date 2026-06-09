0f6f49039c491a01e00238ca20b5545255330f4d
Aishwariya <aishwariyak25csc@vetias.ac.in>
2026-06-09 13:37:04 +0530

    attendance

--- src/day4.java
+++ src/day4.java
@@ -1,4 +1,4 @@
-void main() {
+/*void main() {
     int[][] marks = new int[5][3];
     int[][] studentmarks = {
             {67, 89, 90},
@@ -14,5 +14,27 @@ void main() {
             System.out.print(studentmarks[row][column] + " ");
         }
         System.out.println();
+        }
+}*/
+void main() {
+    int[][] attendance=new int[5][3];
+    int[][] studentattendance={
+            {0,1,0},
+            {1,0,0},
+            {1,1,1},
+            {1,0,1},
+            {0,0,0}
+    };
+    for(int row=0;row< attendance.length;row++){
+        System.out.print("Student"+(1+row)+":");
+        for(int column=0;column<studentattendance[row].length;column++){
+            if(studentattendance[row][column]==1){
+                System.out.println("class"+(1+column)+":present");
+            }else{
+                System.out.println("class"+(1+column)+":absent");
+            }
+        }
+        System.out.println(" ");
     }
+
 }
