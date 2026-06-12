fc538a0aa5447318e65dc7c0f6507035dda42d57
nafeelastar2405-ctrl <nafeelastar2405@gmail.com>
2026-06-12 14:42:19 +0530

    payment

--- src/day6.java
+++ src/day6.java
@@ -1,4 +1,4 @@
-public class day6 {
+/*public class day6 {
     static class car {
         String color;
         String brand;
@@ -28,4 +28,42 @@ public class day6 {
         car car1 = new car("white", "bmw", 500, 300);
         car1.printDetails();
     }
-}
+ }
+}*/
+public class day6 {
+    static class Attributes {
+        String Name;
+        String regNo;
+        String Dept;
+        String Mathsmarks;
+        String physicsmarks;
+        String csemarks;
+
+
+        Attributes() {
+            System.out.println("Constructor called");
+        }
+        Attributes(String Name, String regNo, String Dept, String Mathsmarks,String physicsmarks,String csemarks ){
+            this.Name =Name ;
+            this.regNo= regNo;
+            this. Dept= Dept;
+            this.Mathsmarks=Mathsmarks;
+            this.physicsmarks=physicsmarks;
+            this.csemarks=csemarks;
+        }
+
+        void printDetails() {
+            System.out.println("Attributes");
+            System.out.println("Name:" +Name);
+            System.out.println("regNo:"+regNo);
+            System.out.println("Dept:"+Dept);
+            System.out.println("Mathsmarks:" + Mathsmarks);
+            System.out.println("physicsmarks:" +physicsmarks);
+            System.out.println("csemarks:" + csemarks);
+        }
+    }
+
+    public static void main(String[] args) {
+                      
+    }
+ }
