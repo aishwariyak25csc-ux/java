6b2c333ad195d6e8912256d1a92a2e0339c681a9
supriya <supriya25ai@vetias.ac.in>
2026-06-13 16:05:37 +0530

    Adding progress for Day8

--- src/day8.java
+++ src/day8.java
@@ -1,36 +1,48 @@
-/*public class day8 {
-    record courseRecord(String courseName, String department) {
+/*public class day8{
+    record CourseRecord(int course,int dept){
     }
     static void main(){
-        courseRecord courseRecord=new courseRecord("java","BSC CS");
-        System.out.println(courseRecord.courseName());
-        System.out.println(courseRecord.department());
-
+        CourseRecord courserecord=new CourseRecord(123,34);
+        System.out.println(courserecord.course());
+        System.out.println(courserecord.dept());
     }
-}
-}
- */
-public  class day8 {
+}*/
 
+
+
+
+/*public class day8 {
     enum LANGUAGES{
         EN("English"),
         GR("German"),
         JP("Japanese"),
-        TM("Tamil");
-
-        String value;
-
-        LANGUAGES(String value){
-            this.value=value;
+        TA("Tamil");
+        String description;
+        LANGUAGES(String description){
+            this.description=description;
         }
+
     }
+    static void main(){
+        System.out.println(LANGUAGES.EN.description);
+    }
+}*/
 
+public class day8 {
+    enum DEPARTMENT{
+        CS("computer science"),
+        AI("Artificial intelligence"),
+        AIDS("Artificial intelligence and data science"),
+        CSA("Computer science and applications ");
+        String description;
+        DEPARTMENT(String description){
+            this.description=description;
+        }
 
-
-    static void main() {
-        System.out.println(LANGUAGES.TM.value);
-        System.out.println(LANGUAGES.EN.value);
-        System.out.println(LANGUAGES.GR.value);
-        System.out.println(LANGUAGES.JP.value);
+    }
+    static void main(){
+        System.out.println(DEPARTMENT.CS.description);
     }
 }
+
+
