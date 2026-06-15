d43d41eeeb01ea8b85e4b59d1e3fc0208e3eb8fc
aishwariya <aishwariyak25csc@vetias.ac.in>
2026-06-15 09:37:18 +0530

    login

--- src/day8.java
+++ src/day8.java
@@ -29,7 +29,7 @@
 }*/
 
 public class day8 {
-    enum DEPARTMENT{
+    /*enum DEPARTMENT{
         CS("computer science"),
         AI("Artificial intelligence"),
         AIDS("Artificial intelligence and data science"),
@@ -45,6 +45,35 @@ public class day8 {
     static void main(){
         System.out.println(DEPARTMENT.CS.description);
     }
+}*/
+sealed interface LoginResult permits LoginSuccess, LoginFailure, LoginLoading {
 }
+record LoginSuccess(String time) implements LoginResult {
+}
+record LoginFailure(String reason)  implements LoginResult {
+}
+record LoginLoading(String expectedTime)  implements LoginResult {
+}
+record Random(String reason){
+}
+static void handleLogin(LoginResult result) {
+    switch (result) {
+        case LoginSuccess success -> System.out.println("Login Success at : " + success.time());
+        case LoginFailure failure -> System.out.println("Login Failure because of " + failure.reason());
+        case LoginLoading loading -> System.out.println("Login Loading, " + loading.expectedTime());
+    }
+}
+static void main() {
+    LoginResult LoginSuccess = new LoginSuccess("2026-06-13:03:03:10");
+    LoginResult LoginFailure = new LoginFailure("Insufficient balance");
+    LoginResult LoginLoading = new LoginLoading("Check after 5 hours");
+    handleLogin(LoginSuccess);
+    handleLogin(LoginFailure);
+    handleLogin(LoginLoading);
+}}
+
+
+
+
 
 
