ff0fc3650363d4ed14734af16ba8c67120384d5a
aishwariya <aishwariyak25csc@vetias.ac.in>
2026-06-16 15:16:00 +0530

    remove

--- src/day10.java
+++ src/day10.java
@@ -1,4 +1,6 @@
-import java.util.ArrayList;
+import java.util.LinkedList;
+
+/*import java.util.ArrayList;
 
 public class day10 {
     static void main(){
@@ -17,5 +19,25 @@ public class day10 {
         for(String s:list){
             System.out.print(s);
         }
+    }
+}*/
+import java.util.LinkedList;
+
+public class day10 {
+    static void main() {
+        LinkedList<String> queue = new LinkedList<String>();
+        queue.add("aishu");
+        queue.add("monisha");
+        queue.add("devi");
+        queue.add("gopiha");
+        queue.add("nafeela");
+        while(!queue.isEmpty()) {
+            process(queue);
+        }
+    }
+    static void process(LinkedList<String> queue){
+        String removed=queue.removeFirst();
+        System.out.println("Removed:"+removed);
+
     }
 }
