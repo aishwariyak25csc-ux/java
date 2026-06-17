36fae6bb143422b0c0b5de7e070bc5fc111cb861
aishwariya <aishwariyak25csc@vetias.ac.in>
2026-06-17 16:13:25 +0530

    hashmap

--- src/day11.java
+++ src/day11.java
@@ -1,4 +1,8 @@
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/*import java.util.HashSet;
 import java.util.Set;
 
 public class day11 {
@@ -14,4 +18,16 @@ public class day11 {
          System.out.println(S);
      }
  }
+}*/
+import java.util.HashMap;
+import java.util.HashSet;
+public class day11 {
+    public static void main(String[] args) {
+        HashMap<String, Object> person = new HashMap<>();
+        person.put("ram", "89");
+        person.put("john", "78");
+        person.entrySet().forEach(entry -> {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        });
+    }
 }
