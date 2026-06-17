e31eaf594608e831137dd434adc15532bcab5738
aishwariya <aishwariyak25csc@vetias.ac.in>
2026-06-17 14:20:54 +0530

    set

---
+++ src/day11.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class day11 {
+ public static void main(String[] args){
+     Set<String>set=new HashSet<>();
+     set.add("Student 1");
+     set.add("Student 2");
+     set.add("Student 1");
+     set.add("Student 2");
+     set.add("Student 3");
+     System.out.println(set);
+     for(String S:set){
+         System.out.println(S);
+     }
+ }
+}
